f50686eb52e3e89fbc3362b4eb8f9b5a38d4d535
Rohan Ponnanna K K <96649364+Rohan0603@users.noreply.github.com>
2023-04-30 02:26:05 +0530

    Add files via upload

---
+++ pages/3_Food_Recommendation.py
@@ -0,0 +1,55 @@
+
+import streamlit as st
+import pandas as pd
+import os
+
+# st.sidebar.subheader(st.session_state)
+
+menu = ["South", "North", "East", "West"]
+choice = st.sidebar.selectbox("Choose your region", menu)
+
+
+
+if choice == "South":
+    html_temp = '''
+    <div style="background-color:#f2b016;padding:1.5px">
+    <h1 style="color:white;text-align:center;">South Indian Diet Plan</h1>
+    </div><br>'''
+    st.markdown(html_temp,unsafe_allow_html=True)
+
+    image_filenames = os.listdir('data/South')
+    for filename in image_filenames:
+        st.image(f'data/South/{filename}')
+
+elif choice == "North":
+    html_temp = '''
+    <div style="background-color:#f2b016;padding:1.5px">
+    <h1 style="color:white;text-align:center;">North Indian Diet Plan</h1>
+    </div><br>'''
+    st.markdown(html_temp,unsafe_allow_html=True)
+
+    image_filenames = os.listdir('data/North')
+    for filename in image_filenames:
+        st.image(f'data/North/{filename}')
+
+elif choice == "East":
+    html_temp = '''
+    <div style="background-color:#f2b016;padding:1.5px">
+    <h1 style="color:white;text-align:center;">East Indian Diet Plan</h1>
+    </div><br>'''
+    st.markdown(html_temp,unsafe_allow_html=True)
+
+    image_filenames = os.listdir('data/East')
+    for filename in image_filenames:
+        st.image(f'data/East/{filename}')
+
+elif choice == "West":
+    html_temp = '''
+    <div style="background-color:#f2b016;padding:1.5px">
+    <h1 style="color:white;text-align:center;">West Indian Diet Plan</h1>
+    </div><br>'''
+    st.markdown(html_temp,unsafe_allow_html=True)
+
+    image_filenames = os.listdir('data/West')
+    for filename in image_filenames:
+        st.image(f'data/West/{filename}')
